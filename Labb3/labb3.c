f0ba37ac47dc1e7b5ccf96ac1cbcbf7aa77182a6
Ömer Kolsuz <112700914+omerkolsuz@users.noreply.github.com>
2024-02-26 14:18:29 +0100

    Update labb3.c

--- Labb3/labb3.c
+++ Labb3/labb3.c
@@ -1,60 +1,64 @@
-#include <array>
-#include <cstdint>
-#include <queue>
-
 #include "esp_task_wdt.h"
 #include "driver/dac.h"
 #include "driver/adc.h"
 #include "driver/gpio.h"
 #include "soc/dac_channel.h"
 #include "esp32/rom/ets_sys.h"
+#include "esp_timer.h"
 
-extern "C" auto app_main() -> void;
+//The sampling freq to be used
+int freq = 10000;
+//filtrets längd
+#define M 9
+//initialisera arrays
+static float xbuff[M+1] = {0};
+//koefficienterna
+//static const float b[M+1] = {0.2500,0.2500,0.2500,0.2500};
+static const float b[M+1] = {0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1};
+//Callback for how often you sample.
+static void periodic_timer_callback(void arg)
+{
+    gpio_set_level(GPIO_NUM_14, 1);
 
-constexpr auto M = 3;
-static std::array<float, M + 1> b{0.2500, 0.2500, 0.2500, 0.2500};
-static std::array<float, M + 1> x{};
-
-constexpr std::uint32_t FREQUENCY = 10'000;
-constexpr std::uint64_t US_ONE_S  = 1'000'000;
-constexpr auto PIN                = GPIO_NUM_13;
-
-static auto timer_callback(void arg) -> void {
-    static auto k = 0;
-    x[0] = float(adc1_get_raw(ADC1_CHANNEL_0) / 16);
-    gpio_set_level(PIN, 1U);
-    x[k++] = x[0];
-    if (k == M + 1) k = 0;
-
-    auto sum = 0.0f;
-    for (auto i = M; i >= 0; i--) {
-        sum += b[i] x[i];
+    float val = adc1_get_raw(ADC1_CHANNEL_4);
+    val = val / 16;
+    //Uppdatera xbuff
+    for (int i = M; i > 0; i--) {
+        xbuff[i] = xbuff[i-1];
+    }
+    xbuff[0] = val;
+    //beräkna filtervärdet
+    float filtered_val = 0.0;
+    for (int i = 0; i <= M; i++) {
+        filtered_val += xbuff[i]b[i];
     }
 
-    gpio_set_level(PIN, 0U);
-    dac_output_voltage(DAC_CHANNEL_1, uint8_t(sum));
+    dac_output_voltage(DAC_CHANNEL_1, (int)filtered_val);
+    gpio_set_level(GPIO_NUM_14, 0);
 }
 
-auto app_main() -> void {
-    adc_power_acquire();
-    adc_gpio_init(adc_unit_t(ADC_UNIT_1), adc_channel_t(ADC1_CHANNEL_0));
+void app_main()
+{
+    //init adc and dac
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
-    gpio_pullup_en(GPIO_NUM_36);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
+    gpio_pullup_en(GPIO_NUM_32);
     dac_output_enable(DAC_CHANNEL_1);
 
     gpio_config_t config;
-    config.pin_bit_mask  = (uint64_t) 1 << PIN;
-    config.mode          = gpio_mode_t(GPIO_MODE_DEF_OUTPUT);
-    config.pull_down_en  = GPIO_PULLDOWN_DISABLE;
-    config.pull_up_en    = GPIO_PULLUP_ENABLE;
+    config.pin_bit_mask = 1 << GPIO_NUM_14;
+    config.mode = GPIO_MODE_DEF_OUTPUT;
+    config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    config.pull_up_en = GPIO_PULLUP_ENABLE;
     ESP_ERROR_CHECK(gpio_config(&config));
 
-    esp_timer_create_args_t periodConfig{};
-    periodConfig.callback = timer_callback;
-    periodConfig.name     = "sample_callback";
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &periodic_timer_callback,
+        //name is optional, but may help identify the timer when debugging
+        .name = "periodic"};
 
-    esp_timer_handle_t periodicHandle;
-    ESP_ERROR_CHECK(esp_timer_create(&periodConfig, &periodicHandle));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodicHandle, US_ONE_S / FREQUENCY));
-}
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000/freq));
+
+   }
